0479827d5478a82cf75b69568b4b3a72ca6e6c6b
Charity Parks <tuppercharity@gmail.com>
2019-09-17 02:18:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,7 +22,15 @@ def assign_rooms(array)
   return new_array  
 end
 
+def printer(array)
+  batch_badge_creator(array).each do |badge|
+    puts badge
+  end
+end
 
+def assign_rooms(array).each do |assignment|
+    puts assignment
+  end
 
   
   
