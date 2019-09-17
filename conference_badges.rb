ebfe19a64c4ea8c06c9201d95c53df4b80aee87d
Charity Parks <tuppercharity@gmail.com>
2019-09-17 02:02:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def badge_maker (name)
+   name = "Hello, my name is Arel."
+end
+
+
+def batch_badge_creator (name)
+  name.each do |name|
+  end
+end
+
+def batch_badge_creator (name)
+  name = ["Hello, my name is Johnny."]
+end
+
+def assign_rooms (name, room)
+  assign_rooms = ["Hello, #{name}!  You'll be assigned to #{room}!"]
+end
+  
+#"Hello, Edsger! You'll be assigned to room 1!"
